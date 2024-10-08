4711fde581283a48d7830a3c21bde74e6d71b5ab
selamyemru <selamyemru6@gmail.com>
2024-10-08 14:34:21 +0300

    project setup

--- myapp/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Shega/frontend-qa-selection/myapp/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- myapp/nuxt.config.ts
+++ myapp/nuxt.config.ts
@@ -1,14 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNitroConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
-  compatibilityDate: '2024-10-08',
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
 })
