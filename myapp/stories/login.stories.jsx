a3ad4b8c1e01042d9297010f18a238ea05944688
selamyemru <selamyemru6@gmail.com>
2024-10-08 17:57:07 +0300

    update login strories

--- myapp/stories/login.stories.jsx
+++ myapp/stories/login.stories.jsx
@@ -1,13 +1,24 @@
 
-import Login from '../src/components/Login'; 
+import Login from '../components/Login.vue'; 
 
 export default {
   title: 'Login', 
   component: Login, 
 };
 
-const Template = (args) => <Login {...args} />;
+const Template = (args) => ({
+  components: { Login },
+  setup() {
+    return { args };
+  },
+  template: '<LoginPage v-bind="args" />',
+});
 
 export const Default = Template.bind({});
 Default.args = {
 };
+
+export const WithError = Template.bind({});
+WithError.args = {
+};
+
